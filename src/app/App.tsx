fa8cfe01004312a4a32b9e1e7a22d98b707714ea
denbarabraza <denis.bareischev@gmail.com>
2023-03-22 00:38:29 +0300

    chg: changed dependencies in App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 
 import CircularProgress from '@mui/material/CircularProgress'
 import Container from '@mui/material/Container'
+import { useSelector } from 'react-redux'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import AppBarItem from '../common/components/AppBarItem'
@@ -13,16 +14,18 @@ import { ErrorSnackbar } from 'common/components/ErrorSnackbar'
 import { initializeAppTC } from 'features/auth/authReducer'
 import { Login } from 'features/login/Login'
 import { TodolistItem } from 'features/todos/TodolistItem'
-import { RootDispatch, useAppSelector } from 'store/store'
+import { RootDispatch, RootStoreType, useAppSelector } from 'store/store'
 
 const App = () => {
   const status = useAppSelector<RequestStatusType>(state => state.app.statusApp)
   const isInitialized = useAppSelector<boolean>(state => state.app.isInitialized)
+  const loginName = useSelector<RootStoreType, string>(state => state.auth.loginName)
+  const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
   const dispatch = RootDispatch()
 
   useEffect(() => {
     dispatch(initializeAppTC())
-  }, [])
+  }, [isLoggedIn])
 
   if (!isInitialized) {
     return (
@@ -34,7 +37,7 @@ const App = () => {
 
   return (
     <div>
-      <AppBarItem />
+      <AppBarItem loginName={loginName} isLoggedIn={isLoggedIn} />
       {status === 'loading' && <Loader />}
 
       <Container>
